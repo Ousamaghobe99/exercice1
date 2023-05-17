7e339d3076388fff16206234add500b2956695ae
Ayari Nesrine <121802421+ayarinesrine@users.noreply.github.com>
2023-05-17 13:08:32 +0100

    up

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,30 +1,40 @@
 import React, { Component } from "react";
-import "./TodoList.css";
 export default class TodoList extends Component {
   state = {
+    todoList: [
+      { todo: "étudier", done: true },
+      { todo: "préparer son diner", done: true },
+      { todo: "faire ses prosits", done: false },
+      { todo: "faire ses cources", done: false },
+    ],
     newList: "",
-    list: ""
   };
-  handleAddTask = () => {
-    this.setState({ list: this.state.newList });
-    document.querySelector("li").style.display = "Block";
-  };
+
   handleChangeTask = (e) => {
     this.setState({ newList: e.target.value });
   };
-  handleCompleteTask = () => {
-    document.querySelector("span").innerHTML = "(Terminé)";
+  handleAddTask = () => {
+      var newTodo = { todo: this.state.newList, done: false };
+      var todoListCopy = this.state.todoList;
+      todoListCopy.push(newTodo);
+      this.setState({ todoList: todoListCopy });
+      this.setState({ newList: "" });
   };
-  handleDeleteTask = () => {
-    document.querySelector("li").style.display = "none";
-    this.setState({ list:"" });
-  };
+  handleCompleteTask(index) {
+    var todoListCopy = this.state.todoList;
+    todoListCopy[index].done = true;
+    this.setState({ todoList: todoListCopy });
+  }
+
+  handleDeleteTask(index) {
+    var todoListCopy = this.state.todoList;
+    todoListCopy.splice(index, 1);
+    this.setState({ todoList: todoListCopy });
+  }
 
   render() {
-
     return (
       <div>
-    
         <h1>Ma liste des tàches</h1>
         <div>
           <input
@@ -35,14 +45,27 @@ export default class TodoList extends Component {
           <button onClick={this.handleAddTask}>Ajouter une tàche</button>
         </div>
         <ul>
-         
-          <li>
-            {this.state.list}
-            <span></span>
-            <button onClick={this.handleCompleteTask}>Terminer</button>
-            <button onClick={this.handleDeleteTask}>Supprimer</button>
-          </li>
-          
+          {this.state.todoList.map((e, i) => {
+            return (
+              <li>
+                {e.todo} {e.done === true ? <span>(terminée)</span> : null}{" "}
+                <button
+                  onClick={() => {
+                    this.handleCompleteTask(i);
+                  }}
+                >
+                  Terminer
+                </button>
+                <button
+                  onClick={() => {
+                    this.handleDeleteTask(i);
+                  }}
+                >
+                  Supprimer
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     );
